7684fb1907b951c153b29b9a44687322c0f1db62
xinlin <ssinzlin17@gmail.com>
2023-07-24 12:36:53 +0800

    Remove unnecessary comments from the main program

--- stock.py
+++ stock.py
@@ -15,11 +15,8 @@ class Stock:
         url = f"https://tw.quote.finance.yahoo.net/quote/q?type=ta&perd=d&mkt=10&sym={stock_id}&v=1&callback=jQuery111302872649618000682_1649814120914&_=1649814120915"
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/111.25 (KHTML, like Gecko) Chrome/99.0.2345.81 Safari/123.36'}
         res = requests.get(url,headers=headers)
-        # 最新價格
         current = [l for l in res.text.split('{') if len(l)>=60][-1]
         current = current.replace('"','').split(',')
-        # print(current)
-        # 昨日價格
         yday_all = [l for l in res.text.split('{') if len(l)>=60][-2].split(',')
         yday = float(re.search(':.*',yday_all[4]).group()[1:])
         stock_dict = {'股票代碼':stock_id,
@@ -44,20 +41,18 @@ class Stock:
         url = 'https://notify-api.line.me/api/notify'
         token = data['token']
         headers = {
-            'Authorization': 'Bearer ' + token    # 設定權杖
+            'Authorization': 'Bearer ' + token    
         }
         data = {
-            'message':message     # 設定要發送的訊息
+            'message':message     
         }
-        data = requests.post(url, headers=headers, data=data)   # 使用 POST 方法
+        data = requests.post(url, headers=headers, data=data)   
     
     def main (self,history):
         stock_dict = self.stock_price()
         message = self.check_stock_price_change(stock_dict)
         if stock_dict['漲跌幅']!=history.get(i):
             self.send_notify(message)
-        # else:
-            # self.send_notify('the same')
         return stock_dict
         
         
@@ -73,7 +68,7 @@ if __name__ == '__main__':
             stock_dict = model.main(history)
             history[i] = stock_dict['漲跌幅']
         print('等待五分鐘',datetime.now())
-        time.sleep(300)  # 300 秒等於五分鐘
+        time.sleep(300)  
   
 
         
